2788d03839579c19c68031b0941c863823435210
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-12 21:52:48 +0000

    created timer function

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const process = require('process');
+const times = process.argv.slice(2);
+const timesNumbers = times.map(x => Number(x));
+console.log(timesNumbers);
+for (const time of timesNumbers) {
+  if (typeof time === "number" && time >= 0) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, time * 1000);
+  }
+}
