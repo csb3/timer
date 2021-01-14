c4d5be4132895ef129652ac042e83929352ffa2d
Cass Bredeson <c.s.bredeson@gmail.com>
2021-01-14 03:51:34 +0000

    removed console log

--- timer1.js
+++ timer1.js
@@ -1,7 +1,6 @@
 const process = require('process');
 const times = process.argv.slice(2);
 const timesNumbers = times.map(x => Number(x));
-console.log(timesNumbers);
 for (const time of timesNumbers) {
   if (typeof time === "number" && time >= 0) {
     setTimeout(() => {
